933cb0089cb5d3480c49b2a28550133dd28e5319
haokunyuan <42976264+haokunyuan@users.noreply.github.com>
2025-11-07 04:19:12 +0000

    Deploying to gh-pages from @ haokunyuan/limingjian@55f684aadf7324ca30095674d414c986525589f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/limingjian/";
     },
-  },{id: "nav-",
-          title: "",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/limingjian/cv/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "Research",
           description: "",
           section: "Navigation",
